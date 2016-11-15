541905964d015fa64f52d931fa98886972de99e0
mc007 <asd28071977>
2016-11-15 10:43:22 +0100

    theme updates

--- build/main_build.js
+++ build/main_build.js
@@ -6565,7 +6565,7 @@ define('xdeliteful/MediaPlayer',[
             path = path.replace(/\\/g, '/');
             if(item.isDir) {
                 //path += '/*.mp3';
-                path+='/+(*.mp3|*.wav|*.m4a|*.wma)'
+                path+='/+(*.mp3|*.wav|*.m4a|*.wma)';
             }
             vlc.setVariable("Current Folder",path);
             vlc.callCommand('Play Args');
@@ -6590,7 +6590,7 @@ define('xdeliteful/MediaPlayer',[
                     "deliteful/Combobox",
                     "deliteful/list/List",
                     "dstore/Memory",
-                    "requirejs-dplugins/css!../../../css/xfile/main_lib.css"
+                    "requirejs-dplugins/css!../../../build/xfile/xfile.min.css"
                 ], function (Popup, FileGridLight, utils, DriverStore, types, Deferred, _Popup, FilePicker, declare,popup,Combobox, List, Memory) {
 
                     function createStore(ext) {
@@ -6607,7 +6607,7 @@ define('xdeliteful/MediaPlayer',[
                         var match = thiz.allowAudio ? "(*.m4a)|(*.wav)|(*.mp3)|(*.wma)" : "";
                         if(thiz.allowVideo){
                             thiz.allowAudio && (match+="|");
-                            match+="(*.avi)|(*.mp4)|(*.mkv)"
+                            match+="(*.avi)|(*.mp4)|(*.mkv)";
                         }
                         match+="|!(*.*)";
                         return new DriverStore({
@@ -65357,10 +65357,11 @@ define('xaction/ActionModel',[
 });;
 /** @module xide/model/Path */
 define('xide/model/Path',[
-    "dojo/_base/declare"
-], function (declare) {
-
-    var Path = declare("xide.model.Path", null, {
+    "xide/utils",
+    "dcl/dcl"
+], function (utils, dcl) {
+    var Path = dcl(null, {
+        declaredClass: "xide.model.Path",
         /**
          * @class xide.model.Path
          * @constructor
@@ -65372,13 +65373,13 @@ define('xide/model/Path',[
                 this.getSegments();
             } else {
                 this.segments = path;
-                this.hasLeading = hasLeading!==null ? hasLeading : false;
-                this.hasTrailing = hasTrailing !==null ? hasLeading : false;
+                this.hasLeading = hasLeading !== null ? hasLeading : false;
+                this.hasTrailing = hasTrailing !== null ? hasLeading : false;
             }
         },
 
         endsWith: function (tail) {
-            var segments = dojo.clone(this.segments);
+            var segments = utils.clone(this.segments);
             var tailSegments = (new Path(tail)).getSegments();
             while (tailSegments.length > 0 && segments.length > 0) {
                 if (tailSegments.pop() != segments.pop()) {
@@ -65387,17 +65388,17 @@ define('xide/model/Path',[
             }
             return true;
         },
-
         getExtension: function () {
             if (!this.extension) {
                 this.extension = this.path.substr(this.path.lastIndexOf('.') + 1);
             }
             return this.extension;
         },
-
         segment: function (index) {
             var segs = this.getSegments();
-            if (segs.length < index) return null;
+            if (segs.length < index) {
+                return null;
+            }
             return segs[index];
         },
         /**
@@ -65406,36 +65407,29 @@ define('xide/model/Path',[
          * @param recursive {boolean}
          * @returns {String[]}
          */
-        getChildren:function(items,recursive){
-
+        getChildren: function (items, recursive) {
             var result = [];
-
             var root = this,
                 path = this.toString();
 
-            function addChild(child){
-
-                var _path = typeof child !=='string' ? child.toString() : child;
-                if(_path!==path && result.indexOf(_path)==-1){
+            function addChild(child) {
+                var _path = typeof child !== 'string' ? child.toString() : child;
+                if (_path !== path && result.indexOf(_path) == -1) {
                     result.push(_path);
                 }
             }
 
-            _.each(items,function(item){
-
+            _.each(items, function (item) {
                 var child = new Path(item);
-
                 //root match
-                if(child.startsWith(root)){
-
-                    if(recursive){
+                if (child.startsWith(root)) {
+                    if (recursive) {
                         addChild(child.toString());
-                    }else{
+                    } else {
 
                         var diff = child.relativeTo(path);
-                        if(diff) {
+                        if (diff) {
                             var diffSegments = diff.getSegments();
-
                             //direct child
                             if (diffSegments.length == 1) {
                                 addChild(child);
@@ -65450,9 +65444,6 @@ define('xide/model/Path',[
                                     addChild(parent.toString());
                                 }
                             }
-                        }else{
-                            //happens with child==this is the same path
-                            //console.error('cant build diff for ' + child.toString() + ' and path : ' + path);
                         }
                     }
 
@@ -65477,24 +65468,20 @@ define('xide/model/Path',[
             }
             return this.segments;
         },
-
         isAbsolute: function () {
             return this.hasLeading;
         },
-
         getParentPath: function () {
             if (!this._parentPath) {
-                var parentSegments = dojo.clone(this.segments);
+                var parentSegments = utils.clone(this.segments);
                 parentSegments.pop();
                 this._parentPath = new Path(parentSegments, this.hasLeading);
             }
-            return dojo.clone(this._parentPath);
+            return utils.clone(this._parentPath);
         },
-
         _clone: function () {
-            return new Path(dojo.clone(this.segments), this.hasLeading, this.hasTrailing);
+            return new Path(utils.clone(this.segments), this.hasLeading, this.hasTrailing);
         },
-
         append: function (tail) {
             tail = tail || "";
             if (typeof tail == 'string') {
@@ -65512,7 +65499,6 @@ define('xide/model/Path',[
             }
             return result;
         },
-
         toString: function () {
             var result = [];
             if (this.hasLeading) {
@@ -65529,14 +65515,13 @@ define('xide/model/Path',[
             }
             return result.join("");
         },
-
         removeRelative: function () {
             var segs = this.getSegments();
-            if (segs.length > 0 && segs[1] == ".")
+            if (segs.length > 0 && segs[1] == ".") {
                 return this.removeFirstSegments(1);
+            }
             return this;
         },
-
         relativeTo: function (base, ignoreFilename) {
             if (typeof base == 'string') {
                 base = new Path(base);
@@ -65565,16 +65550,13 @@ define('xide/model/Path',[
             }
             return new Path(newSegments, false, this.hasTrailing);
         },
-
         startsWith: function (anotherPath) {
             var count = this.matchingFirstSegments(anotherPath);
             return anotherPath._length() == count;
         },
-
         _length: function () {
             return this.segments.length;
         },
-
         matchingFirstSegments: function (anotherPath) {
             var mySegments = this.segments;
             var pathSegments = anotherPath.getSegments();
@@ -65588,36 +65570,29 @@ define('xide/model/Path',[
             }
             return count;
         },
-
         removeFirstSegments: function (count) {
             return new Path(this.segments.slice(count, this.segments.length), this.hasLeading, this.hasTrailing);
         },
-
         removeMatchingLastSegments: function (anotherPath) {
             var match = this.matchingFirstSegments(anotherPath);
             return this.removeLastSegments(match);
         },
-
         removeMatchingFirstSegments: function (anotherPath) {
             var match = this.matchingFirstSegments(anotherPath);
             return this._clone().removeFirstSegments(match);
         },
-
         removeLastSegments: function (count) {
             if (!count) {
                 count = 1;
             }
             return new Path(this.segments.slice(0, this.segments.length - count), this.hasLeading, this.hasTrailing);
         },
-
         lastSegment: function () {
             return this.segments[this.segments.length - 1];
         },
-
         firstSegment: function (length) {
             return this.segments[length || 0];
         },
-
         equals: function (anotherPath) {
             if (this.segments.length != anotherPath.segments.length) {
                 return false;
@@ -65629,9 +65604,7 @@ define('xide/model/Path',[
             }
             return true;
         },
-
         _canonicalize: function () {
-
             var doIt;
             var segments = this.segments;
             for (var i = 0; i < segments.length; i++) {
